1e6f147aa3b6c81612920b8189bb4fc3738a6221
Johnbush <johnbush20@gmail.com>
2019-03-22 11:57:17 +0530

    update function Name

--- builds/jazz-build-module/utility-loader.groovy
+++ builds/jazz-build-module/utility-loader.groovy
@@ -220,11 +220,7 @@ def getAccountBucketName(service_config) {
 *  Get Account Specific API Gateway
 */
 
-<<<<<<< HEAD
 def getApiId(service_config) {
-=======
-def getAccountAPIName(service_config) {
->>>>>>> delete build pack changes
 	def apiGateway = {}
 	def accountObject = getAccountInfo(service_config);
 	if( accountObject.size() > 0){
